be050908f8479b91d23f8afc379ffebe0b28154f
Brishna Bakshev <b.bakshev@gmail.com>
2023-02-24 12:08:39 -0800

    WIP: lunch and then fix the error

--- PierresBakery/Models/Coffee.cs
+++ PierresBakery/Models/Coffee.cs
@@ -2,8 +2,13 @@ namespace PierresBakery.Models
 {
   public class Coffee
   {
-    public int NumberOfCofees{get; set;}
+    public int NumberOfCofees {get; set;}
+    public decimal CostPerCoffe {get; set;}
 
-    
+    public decimal TotalCostCoffee()
+    {
+      return NumberOfCofees * CostPerCoffe;
+    }
+
   }
 }
