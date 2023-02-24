bf535678584a7bd77377b7d2f268dfa0db68fb42
Brishna Bakshev <b.bakshev@gmail.com>
2023-02-24 09:15:45 -0800

    passed - Total cost of for a single loaf returns the value of 5

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -3,5 +3,11 @@ namespace PierresBakery.Models
   public class Bread
   {
     public int NumberOfLoaves{get; set;}
+
+    public double TotalCost()
+    {
+      int singleLoafOfBread = NumberOfLoaves % 3;
+      return singleLoafOfBread * 5;
+    }
   }
 }
