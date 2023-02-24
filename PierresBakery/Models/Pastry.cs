a95a092556c4f410f0b2282e2f25046558e3ef80
Brishna Bakshev <b.bakshev@gmail.com>
2023-02-24 10:38:42 -0800

    passed - single pastry cost has been added in the business logic

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -3,5 +3,11 @@ namespace PierresBakery.Models
   public class Pastry
   {
     public int NumberOfPastries {get; set;}
+
+    public double TotalCostPastry()
+    {
+      int singlePastry = NumberOfPastries % 4;
+      return singlePastry * 2;
+    }
   }
 }
